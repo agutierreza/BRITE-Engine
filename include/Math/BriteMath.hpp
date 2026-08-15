4a9e8bd460e01b39ee5398892b866228270c0210
agutierreza <gutialco@gmail.com>
2026-08-15 22:15:11 +0100

    feat: add basic vector math operations
    - Added operator overloads and basic vector functions to BriteMath.hpp to decouple from raymath.h.
    - Added <type_traits> include to support MSYS2 C++20 <compare> header dependencies.

--- include/Math/BriteMath.hpp
+++ include/Math/BriteMath.hpp
@@ -1,20 +1,128 @@
 #pragma once
 
+#include <cmath>
 #include <cstdint>
+#include <type_traits>
 
 namespace BRITE {
 namespace Math {
 
+// TODO: Next time we need to add anything to this math library, we had better just include GLM!
+
 struct Vector2 {
     float x;
     float y;
+
+    Vector2& operator+=(const Vector2& rhs) {
+        x += rhs.x;
+        y += rhs.y;
+        return *this;
+    }
+    Vector2& operator-=(const Vector2& rhs) {
+        x -= rhs.x;
+        y -= rhs.y;
+        return *this;
+    }
+    Vector2& operator*=(float rhs) {
+        x *= rhs;
+        y *= rhs;
+        return *this;
+    }
+    Vector2& operator/=(float rhs) {
+        x /= rhs;
+        y /= rhs;
+        return *this;
+    }
+    Vector2 operator-() const {
+        return {-x, -y};
+    }
 };
+
+inline Vector2 operator+(const Vector2& a, const Vector2& b) {
+    return {a.x + b.x, a.y + b.y};
+}
+inline Vector2 operator-(const Vector2& a, const Vector2& b) {
+    return {a.x - b.x, a.y - b.y};
+}
+inline Vector2 operator*(const Vector2& a, float b) {
+    return {a.x * b, a.y * b};
+}
+inline Vector2 operator*(float a, const Vector2& b) {
+    return {a * b.x, a * b.y};
+}
+inline Vector2 operator/(const Vector2& a, float b) {
+    return {a.x / b, a.y / b};
+}
 
 struct Vector3 {
     float x;
     float y;
     float z;
+
+    Vector3& operator+=(const Vector3& rhs) {
+        x += rhs.x;
+        y += rhs.y;
+        z += rhs.z;
+        return *this;
+    }
+    Vector3& operator-=(const Vector3& rhs) {
+        x -= rhs.x;
+        y -= rhs.y;
+        z -= rhs.z;
+        return *this;
+    }
+    Vector3& operator*=(float rhs) {
+        x *= rhs;
+        y *= rhs;
+        z *= rhs;
+        return *this;
+    }
+    Vector3& operator/=(float rhs) {
+        x /= rhs;
+        y /= rhs;
+        z /= rhs;
+        return *this;
+    }
+    Vector3 operator-() const {
+        return {-x, -y, -z};
+    }
 };
+
+inline Vector3 operator+(const Vector3& a, const Vector3& b) {
+    return {a.x + b.x, a.y + b.y, a.z + b.z};
+}
+inline Vector3 operator-(const Vector3& a, const Vector3& b) {
+    return {a.x - b.x, a.y - b.y, a.z - b.z};
+}
+inline Vector3 operator*(const Vector3& a, float b) {
+    return {a.x * b, a.y * b, a.z * b};
+}
+inline Vector3 operator*(float a, const Vector3& b) {
+    return {a * b.x, a * b.y, a * b.z};
+}
+inline Vector3 operator/(const Vector3& a, float b) {
+    return {a.x / b, a.y / b, a.z / b};
+}
+
+inline float DotProduct(const Vector3& a, const Vector3& b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+inline Vector3 CrossProduct(const Vector3& a, const Vector3& b) {
+    return {a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x};
+}
+
+inline float Length(const Vector3& v) {
+    return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+inline Vector3 Normalize(const Vector3& v) {
+    float len = Length(v);
+    if (len > 0.0f) {
+        return v / len;
+    }
+    return v;
+}
 
 struct Quaternion {
     float x;
